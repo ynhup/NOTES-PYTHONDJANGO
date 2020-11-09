d804affb79ba73e3577c6d2e22c8be47562c5915
ynhup1999 <55817624+ynhup1999@users.noreply.github.com>
2020-11-09 21:21:28 +0700

    sourcecode

---
+++ todos/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class task(models.Model):
+    title = models.CharField(max_length=200)
+    complete = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+    due = models.DateTimeField(auto_now_add=False, auto_now=False, blank=True, null=True)
+    def __str__(self):
+        return self.title
